36dc4aefea80cacb3ad34432f64337abeb77da1c
julio <juliocesar.melegari@gmail.com>
2025-06-09 22:36:31 -0300

    add User Role model

---
+++ src/main/java/com/jcm/ecomerce/entities/Role.java
@@ -0,0 +1,43 @@
+package com.jcm.ecomerce.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_role")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String authority;
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(authority);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(authority, other.authority);
+	}
+}
--- src/main/java/com/jcm/ecomerce/entities/User.java
+++ src/main/java/com/jcm/ecomerce/entities/User.java
@@ -2,13 +2,18 @@ package com.jcm.ecomerce.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -46,5 +51,25 @@ public class User {
 	@Setter(AccessLevel.NONE)
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
+	
+	@Setter(AccessLevel.NONE)
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+				joinColumns = @JoinColumn(name = "user_id"),
+				inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+	
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+	
+	public boolean hasRole(String roleNome) {
+		for(Role role : roles) {
+			if(role.getAuthority().equals(roleNome)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
