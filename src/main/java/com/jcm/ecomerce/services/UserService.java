df910e43370d4e46448980386a9d90309d1fbdf5
julio <juliocesar.melegari@gmail.com>
2025-06-10 00:20:26 -0300

    add projection, add method searchUserAndRolesByEmail in UserRepository, refactored method loadUserByUsername in UserService

--- src/main/java/com/jcm/ecomerce/services/UserService.java
+++ src/main/java/com/jcm/ecomerce/services/UserService.java
@@ -1,12 +1,16 @@
 package com.jcm.ecomerce.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.jcm.ecomerce.entities.Role;
 import com.jcm.ecomerce.entities.User;
+import com.jcm.ecomerce.projections.UserDetailsProjection;
 import com.jcm.ecomerce.repositories.UserRepository;
 
 @Service
@@ -17,9 +21,16 @@ public class UserService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User user = repository.findByEmail(username);
-		if(user == null) {
+		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+		if(result.size() == 0) {
 			throw new UsernameNotFoundException("User not found");
+		}
+		
+		User user = new User();
+		user.setEmail(username);
+		user.setPassword(result.get(0).getPassword());
+		for(UserDetailsProjection projection : result) {
+			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
 		}
 		return user;
 	}
