552931415a5da3d39961c781d825148fa2f4fbe7
julio <juliocesar.melegari@gmail.com>
2025-06-14 16:21:37 -0300

    OAuth2, JWT, password grant

--- src/main/java/com/jcm/ecomerce/EcomerceApplication.java
+++ src/main/java/com/jcm/ecomerce/EcomerceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcomerceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(EcomerceApplication.class, args);
 	}
 
